a8ee444c00f50380450c201e1c6a47b95e85324d
Ijerry-cloud <jerrychinedu65@gmail.com>
2022-06-26 20:25:00 -0700

    allowed hosts changed

--- RecreatingValues/urls.py
+++ RecreatingValues/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('men-wey-sabi/', admin.site.urls),
     path('homepage/', include('home.urls', namespace='home')),
     path('events/', include('events.urls', namespace='events')),
     path('projects/', include('projects.urls', namespace='projects')),
